1c944d74c8cf7387cd45d9774ef04b169e6fb12a
an.lam <an.lam@sintef.com>
2024-04-18 15:37:54 +0200

    clean files

--- util/log.py
+++ util/log.py
@@ -1,6 +1,6 @@
 import logging
 
-FORMAT = "%(asctime)s CT-ONTO-LIB: [%(filename)s - %(funcName)s] %(message)s"
+FORMAT = "%(asctime)s SINDIT: [%(filename)s - %(funcName)s] %(message)s"
 logging.basicConfig(format=FORMAT)
-logger = logging.getLogger("ct-onto-lib")
+logger = logging.getLogger("sindit")
 logger.setLevel(logging.INFO)
