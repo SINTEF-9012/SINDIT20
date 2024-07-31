5420aa5074de6c286fcf8c3857afd014fcacc953
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-07-31 14:49:18 +0200

    update: more tests

--- tests/test_connectors.py
+++ tests/test_connectors.py
@@ -31,20 +31,30 @@ class TestMQTTConnector:
 class TestInfluxDBConnector:
     """Test the InfluxDBConnector class."""
 
-    bucket = "test_bucket"
-
     def setup_method(self):
-        self.influx = InfluxDBConnector(bucket=self.bucket)
+        self.influx = InfluxDBConnector()
 
     def test_init(self):
         """Test default values of InfluxDBConnector."""
         assert self.influx.host == "localhost"
         assert self.influx.port == str(8086)
         assert self.influx.org is None
-        assert self.influx.bucket is self.bucket
 
     def test_set_token(self):
         """Test the set_token method of InfluxDBConnector."""
         token = "my_token"
         self.influx.set_token(token)
         assert self.influx._InfluxDBConnector__token == token
+
+    def test_set_bucket(self):
+        """Test the set_bucket method of InfluxDBConnector."""
+        bucket = "new_bucket"
+        self.influx.set_bucket(bucket)
+        assert self.influx.bucket == bucket
+
+    def test__check_if_bucket_name_is_set(self):
+        """Test the _check_if_bucket_name_is_set method of InfluxDBConnector."""
+        with pytest.raises(ValueError):
+            self.influx._check_if_bucket_name_is_set()
+        self.influx.set_bucket("new_bucket")
+        assert self.influx._check_if_bucket_name_is_set()
