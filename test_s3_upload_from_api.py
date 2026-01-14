43d8232577e072faa296e501d9c798851ff08e96
An Lam <an.lam@sintef.no>
2026-01-14 11:53:52 +0100

    fix: update API_BASE_URL and NODE_URI for S3 upload test script

--- test_s3_upload_from_api.py
+++ test_s3_upload_from_api.py
@@ -7,8 +7,8 @@ import time
 import json
 
 # Configuration
-API_BASE_URL = "http://localhost:9017"  # Adjust to your API URL
-NODE_URI = "http://sindit.sintef.no/quasar-plim-test/s3/test-node"
+API_BASE_URL = "https://sindit2-api.sintef.cloud/"  # Adjust to your API URL
+NODE_URI = "http://sindit.sintef.no/quasar-plim-test/s3-object/fabd3332-8b44-4968-8f9c-7b501f023024/attachments/image_01"
 USERNAME = "quasar"  # Adjust to your username
 PASSWORD = "quasar"  # Adjust to your password
 
